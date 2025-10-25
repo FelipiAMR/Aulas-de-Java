4a32c79d3c306e45c93163669e92b0a7a0d325f2
FelipiAMR <felipiamorim14@gmail.com>
2025-10-25 15:51:49 -0300

    Login em Java

--- AtividadesTeste.java
+++ AtividadesTeste.java
@@ -4,11 +4,39 @@ public class AtividadesTeste {
     public static void main(String[] args){
         Scanner entrada = new Scanner(System.in);
 
-        System.out.print("Digite seu nome: ");
-        String nome = entrada.nextLine();
+        System.out.print("Digite seu e-mail: ");
+        String email = entrada.nextLine();
 
-        System.out.println("Olá, " + nome + "! Seja bem-vindo ao Java!");
+        System.out.print("Digite sua senha: ");
+        String senha = entrada.nextLine();
+
+        System.out.println("Login realizado com sucesso!");
+        System.out.println("E-mail utilizado: " + email);
+        System.out.println("Senha digitada: " + senha);
+
+        if (email.equals("felipiamorim14@gmail.com") && senha.equals("Angelaudo2400#")) {
+            System.out.println("Login Autorizado");
+        } else {
+            System.out.println("Acesso Negado!");
+        }
+
         entrada.close();
     }
     
+}
+
+class Nome{
+    public static void main(String[] args) {
+        double preco = 100;
+        double desconto = 0.5;
+        double precoFinal = preco * desconto;
+        
+        System.out.println(precoFinal);
+
+        if (precoFinal >= preco){
+            System.out.println("Tivemos um erro no Sistema, tente novamente mais tarde! Por favor!");
+        } else {
+            System.out.println("Seu produto foi descontado com sucesso! Aproveite e volte sempre!");
+        }
+    }
 }
